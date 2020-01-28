9803d112357b5b7a5520647f517d72bb8a9aefb6
brilliantOh <49480475+brilliantOh@users.noreply.github.com>
2020-01-28 16:39:21 +0900

    draft.py and menu_excel

--- project_draft.py
+++ project_draft.py
@@ -11,7 +11,6 @@ for i in quantity_list:
 
 total_amount
 
-
 #dic
 menu_list = ['아메리카노', '카페라떼'] #메뉴(리스트)
 price_dic = {'아메리카노':4100, '카페라떼':4500} #메뉴-가격(딕셔너리)
@@ -26,22 +25,33 @@ def price_mul_quantity():
 
 price_mul_quantity()
 
-#Class?
-menu_list = ['아메리카노', '카페라떼'] #메뉴(리스트)
-price_dic = {'아메리카노':4100, '카페라떼':4600} #메뉴-가격(딕셔너리)
-quantity_dic = {'아메리카노':0, '카페라떼':0} #메뉴-수량(딕셔너리)
+#'menu_excel' load
+import os
+import pandas as pd
 
+filepath = r'D:\Documents\GitHub\self-study\menu_excel.xlsx'
+menu_excel = pd.read_excel(filepath)
+menu_excel
+
+#'menu_excel' -> dictionary
+menu_dic = {}
+
+
+#주문정보 Class (모두 0일 때, 양수 값이 있을 때?)
+#메뉴별 수량을 무슨 type으로 받아서 저장할까? list? dictionary?
 class Menu_Quantity:
     def __init__(self):
+        pass #생성자: 전메뉴 수량 0 세팅
+    def input_quantity(self):
+        pass #수량(양수)
+    def change_quantity(self):
+        pass #수량 추가/변경/제거(int)
+        #수량 output=0일 때 주문현황에서 메뉴 제거
+        #수량 output<0일 수 없음
 
-
-def quantity_input():
-    pass
-
-def price_mul_quantity():
-    total_price = 0  # 주문합계
-    for menu in menu_list: #문제: 수량변경 불가능
-        total_price += quantity_dic[menu] * price_dic[menu]
-    return total_price
-
-price_mul_quantity()
+#최종금액 Class(수량 mul 가격)
+class Total_amount:
+    def __init__(self):
+        pass #생성자: 0 세팅
+    def calculate_total(self): #수량 정보가 input되면
+        pass #메뉴별 수량x메뉴별 가격 sum
