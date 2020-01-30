56c9fb225dadd0bd648326b3522f53984a1a9894
brilliantOh <49480475+brilliantOh@users.noreply.github.com>
2020-01-30 18:38:14 +0900

    draft.py Menu_quantity

--- project_draft.py
+++ project_draft.py
@@ -33,21 +33,30 @@ filepath = r'D:\Documents\GitHub\self-study\menu_excel.xlsx'
 menu_excel = pd.read_excel(filepath)
 menu_excel
 
-#'menu_excel' -> dictionary
-menu_dic = {}
-
-
 #주문정보 Class (모두 0일 때, 양수 값이 있을 때?)
 #메뉴별 수량을 무슨 type으로 받아서 저장할까? list? dictionary?
-class Menu_Quantity:
-    def __init__(self):
-        pass #생성자: 전메뉴 수량 0 세팅
-    def input_quantity(self):
-        pass #수량(양수)
-    def change_quantity(self):
-        pass #수량 추가/변경/제거(int)
+class Menu_quantity:
+    def __init__(self): #생성자: 전메뉴 수량 0 세팅
+        quantity_list = [0,0,0,0]
+        self.quantity_list = quantity_list
+    def input_quantity(self, positive_list):
+        if 1 in positive_list: #처음: 항상 1 값 하나가 들어옴
+            quantity_list = positive_list
+            return quantity_list
+    def change_quantity(self, quantity_list, int_list): #수량 추가/변경/제거(int)
+        for idx in range(len(int_list)):
+            if int_list[idx] == 0:
+                pass
+            elif int_list[idx] != 0:
+                quantity_list[idx] += int_list[idx]
+        return quantity_list
         #수량 output=0일 때 주문현황에서 메뉴 제거
         #수량 output<0일 수 없음
+
+test = Menu_quantity()
+test.input_quantity([1,0,0,0]) #최초 입력
+test.change_quantity([1,0,0,0], [-1,0,1,0]) #quantity_list를 자동으로 input할 수 없을까?
+
 
 #최종금액 Class(수량 mul 가격)
 class Total_amount:
