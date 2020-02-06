099448d502ef1098267dc6c4f19d05b09f23cd13
seohyeon.oh <seohyeon.oh@seizedata.com>
2020-02-06 19:01:52 +0900

    GUI build_200206

--- GUI_draft.py
+++ GUI_draft.py
@@ -1,7 +1,8 @@
 #GUI_draft
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QInputDialog,\
+    QHBoxLayout, QVBoxLayout
 
 
 class MyApp(QWidget):
@@ -10,23 +11,68 @@ class MyApp(QWidget):
         self.initUI()
 
     def initUI(self):
-        btn1 = QPushButton('아메리카노(HOT)', self) #pushbutton #메뉴
-        #btn1.clicked.connect(self.showDialog)  #clicked signal -> call
+        self.menu1_count = 0
+        self.btn1 = QPushButton('아메리카노(HOT)', self) #pushbutton #메뉴
+        self.btn1.clicked.connect(self.menuClicked)  #clicked signal -> call menuClicked
 
-        lbl1 = QLabel('수량', self) #label #수량
-        lbl2 = QLabel('0', self) #수량 count
+        self.btn2 = QPushButton('-', self) #수량-
+        self.btn2.clicked.connect(self.menuMinus)
+        self.btn3 = QPushButton('+', self) #수량+
+        self.btn3.clicked.connect(self.menuAdd)
 
-        vbox = QVBoxLayout()
-        vbox.addWidget(btn1)
-        vbox.addWidget(lbl1)
-        vbox.addWidget(lbl2)
-        vbox.addStretch() #창 크기가 변화해도 stretch 유지
+        self.btn4 = QPushButton('직접입력', self) #수량변경
+        self.btn4.clicked.connect(self.menuInputDialog)
 
-        self.setLayout(vbox) #수직vertical
+        self.btn5 = QPushButton('제거', self) #제거
+        self.btn5.clicked.connect(self.menuCancel)
+
+        self.lbl1 = QLabel('0', self) #label #수량count #default 0
+        self.lbl2 = QLabel('아메리카노(HOT)', self) #주문현황의 메뉴
+
+        hbox = QHBoxLayout() #주문현황 part #HorizentalBox 수평 레이아웃
+        hbox.addWidget(self.lbl2)
+        hbox.addWidget(self.btn2)
+        hbox.addWidget(self.lbl1)
+        hbox.addWidget(self.btn3)
+        hbox.addWidget(self.btn4)
+        hbox.addWidget(self.btn5)
+
+        vbox = QVBoxLayout() #VerticalBox 수직 레이아웃
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+        vbox.addWidget(self.btn1) #메뉴 part
+
+        self.setLayout(vbox) #Layout
 
         self.setWindowTitle('POS by brilliantOh')
-        self.setGeometry(300,300,400,300)
+        self.setGeometry(300,300,300,200)
         self.show()
+
+    def menuClicked(self):
+        self.menu1_count += 1
+        self.lbl1.setText(str(self.menu1_count))
+
+    def menuMinus(self):
+        self.menu1_count -= 1
+        if self.menu1_count < 0:
+            self.menu1_count = 0
+        self.lbl1.setText(str(self.menu1_count))
+
+    def menuAdd(self): #수량+
+        self.menu1_count += 1
+        self.lbl1.setText(str(self.menu1_count))
+
+    def menuInputDialog(self): #수량변경
+        number, ok = QInputDialog.getInt(self, '수량 직접입력', '수량을 입력하세요.', min=0)
+
+        if ok:
+            self.menu1_count = number
+            self.lbl1.setText(str(self.menu1_count))
+
+    def menuCancel(self):
+        self.menu1_count = 0
+        self.lbl1.setText(str(self.menu1_count))
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
