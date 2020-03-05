c4c4be014719c461d092f08a36a8150a5c58afdc
seohyeon.oh <seohyeon.oh@seizedata.com>
2020-03-05 19:25:51 +0900

    reform_200305
    전체 수량,금액 계산 기능
    GUI 일부 구현

--- GUI_reform.py
+++ GUI_reform.py
@@ -1,6 +1,6 @@
 #GUI_reform
 
-from initialize_reform import menu_names_list, Quantity_calculator
+from initialize_reform import POS_calculator, menu_excel, cal, menu_names_list, menu_cost_dic
 import sys
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QTabWidget, QTableWidget, QTableWidgetItem, \
@@ -39,12 +39,12 @@ class MyWidget(QWidget):
         self.initUI()
 
     def initUI(self):
-        # TabWidget call
         tabs = QTabWidget()
+        vbox = QVBoxLayout()
+
         tabs.addTab(FirstTab(), '주문/계산')
         tabs.addTab(SecondTab(), '내역조회')
 
-        vbox = QVBoxLayout()
         vbox.addWidget(tabs)
 
         self.setLayout(vbox)
@@ -57,20 +57,153 @@ class FirstTab(QWidget):
         self.initUI()
 
     def initUI(self):
-        # Groupbox call
         grid = QGridLayout()
-        grid.addWidget(self.createGB0(), 0, 0)
-        grid.addWidget(self.createGB1(), 0, 1)
+
+        grid.addWidget(self.create_order_groupbox(), 0, 0)
+        grid.addWidget(self.create_menu_groupbox(), 0, 1)
 
         self.setLayout(grid)
 
     # Groupbox: 주문현황
-    def create_groupbox_00(self):
-        gbox = QGroupBox('주문현황')
+    def create_order_groupbox(self):
+        gbox = QGroupBox()
+        gbox.setTitle('주문현황')
         hbox = QHBoxLayout()
         vbox = QVBoxLayout()
 
-        # widget 정의
-        self.table = QTableWidget()
-        self.createTable()
-        vbox.addWidget(self.table)
+        # widget
+        self.cart_table = QTableWidget()
+        self.create_cart_table()
+        vbox.addWidget(self.cart_table)
+
+        vbox.addLayout(hbox)
+        btn_cancel_all = QPushButton('전체취소', self)
+        btn_cancel_all.clicked.connect(self.cancel_all_in_cart)
+        hbox.addWidget(btn_cancel_all)
+
+        gbox.setLayout(vbox)
+
+        return gbox
+
+
+    # Groupbox: 메뉴선택
+    def create_menu_groupbox(self):
+        gbox = QGroupBox()
+        gbox.setTitle('메뉴선택')
+        vbox = QVBoxLayout()
+
+        # widget
+        btns_menu = []
+
+        for i in range(len(menu_excel['메뉴명'])):
+            btns_menu.append(QPushButton())
+            btns_menu[i].setText(menu_excel['메뉴명'][i] + '\n' + str(menu_excel['가격'][i]) + '원')
+            vbox.addWidget(btns_menu[i])
+            # Signal
+            btns_menu[i].clicked.connect(lambda arg, idx=i: self.add_menu_in_cart(menu_excel['메뉴명'][idx]))
+
+        gbox.setLayout(vbox)
+
+        return gbox
+
+    # Table Widget: 카트
+    def create_cart_table(self):
+        header_list = ['메뉴', '수량', '감소', '증가', '직접입력', '단가', '금액', '취소']
+
+        self.cart_table.setColumnCount(len(header_list))
+        self.cart_table.setRowCount(len(menu_excel['메뉴명']))
+
+        self.cart_table.setHorizontalHeaderLabels(header_list)
+
+    # Slot
+    @pyqtSlot()
+    def set_item_in_cart_table(self):
+        self.cart_table.clearContents()
+
+        if len(cal.cart_list) != 0:
+            skip_count = 0
+            for i in range(len(cal.cart_list)):
+                if i == 0:
+                    self.cart_table.setItem(0, 0, QTableWidgetItem(cal.cart_list[i]))
+                    self.cart_table.setItem(0, 1, QTableWidgetItem(str(cal.return_menu_qty(cal.cart_list[i]))))
+                    self.cart_table.setItem(0, 5, QTableWidgetItem(str(cal.return_menu_cost(cal.cart_list[i]))))
+                    self.cart_table.setItem(0, 6, QTableWidgetItem(str(cal.return_menu_amount(cal.cart_list[i]))))
+                elif i >= 1 and cal.cart_list[i] in cal.cart_list[:i]:
+                    skip_count += 1
+                else:
+                    self.cart_table.setItem(skip_count+1, 0, QTableWidgetItem(cal.cart_list[i]))
+                    self.cart_table.setItem(skip_count+1, 1, QTableWidgetItem(str(cal.return_menu_qty(cal.cart_list[i]))))
+                    self.cart_table.setItem(skip_count+1, 5, QTableWidgetItem(str(cal.return_menu_cost(cal.cart_list[i]))))
+                    self.cart_table.setItem(skip_count+1, 6, QTableWidgetItem(str(cal.return_menu_amount(cal.cart_list[i]))))
+
+    @pyqtSlot(str)
+    def add_menu_in_cart(self, menu_str):
+        cal.add_qty(menu_str)
+        self.set_item_in_cart_table()
+
+    @pyqtSlot(str)
+    def minus_menu_in_cart(self, menu_str):
+        cal.minus_qty(menu_str)
+        self.set_item_in_cart_table()
+
+    @pyqtSlot(str, int)
+    def input_menu_in_cart(self, menu_str, input_int):
+        cal.input_qty(menu_str, input_int)
+        self.set_item_in_cart_table()
+
+    @pyqtSlot(str)
+    def cancel_menu_in_cart(self, menu_str):
+        cal.cancel_qty(menu_str)
+        self.set_item_in_cart_table()
+
+    @pyqtSlot()
+    def cancel_all_in_cart(self):
+        cal.cancel_all_qty()
+        self.set_item_in_cart_table()
+
+    @pyqtSlot()
+    def print_bill_of_recent_order(self):
+        pass
+
+    @pyqtSlot()
+    def pay_in_cash(self):
+        pass
+
+    @pyqtSlot()
+    def pay_in_credit_card(self):
+        pass
+
+
+# Tab Widget: 내역조회
+class SecondTab(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        grid = QGridLayout()
+
+        grid.addWidget(self.create_choice_groupbox(), 0, 0)
+        grid.addWidget(self.create_view_groupbox(), 0, 1)
+
+        self.setLayout(grid)
+
+    # Groupbox: 주문결제내역
+    def create_choice_groupbox(self):
+        gbox = QGroupBox()
+        gbox.setTitle('주문결제내역')
+
+        return gbox
+
+    # Groupbox: 조회
+    def create_view_groupbox(self):
+        gbox = QGroupBox()
+        gbox.setTitle('조회')
+
+        return gbox
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
--- initialize_reform.py
+++ initialize_reform.py
@@ -8,42 +8,67 @@ excel_path = r'D:\Documents\repository'
 excel_name = 'menu_excel.xlsx'
 menu_excel = pd.read_excel(excel_path + os.sep + excel_name, index_col=None, header=0)
 
-# create menu names list
-menu_names_list = []
-for cell in menu_excel['메뉴명']:
-    menu_names_list.append(cell)
-
 # calculate menu quantity
-class Quantity_calculator:
+class POS_calculator:
     def __init__(self):
         self.cart_list = []
 
     def add_qty(self, menu_str):
         self.cart_list.append(menu_str)
+        return self.cart_list
 
     def minus_qty(self, menu_str):
         if self.cart_list.count(menu_str) != 0:
             self.cart_list.remove(menu_str)
+            return self.cart_list
 
     def cancel_qty(self, menu_str):
         while self.cart_list.count(menu_str) != 0:
             self.cart_list.remove(menu_str)
+        return self.cart_list
 
-    def input_qty(self, menu_str, qty_int):
-        while self.cart_list.count(menu_str) != qty_int:
+    def input_qty(self, menu_str, input_int):
+        while self.cart_list.count(menu_str) != input_int:
             self.cart_list.append(menu_str)
+        return self.cart_list
 
     def cancel_all_qty(self):
         self.cart_list.clear()
+        return self.cart_list
 
-cal = Quantity_calculator()
+    def return_menu_qty(self, menu_str):
+        return self.cart_list.count(menu_str)
 
-menu_amount_list = []
-total_amount_int = 0
-menu_qty_list = []
-for menu in menu_names_list:
-    cal.cart_list.count(menu)
+    def return_total_qty(self):
+        return len(self.cart_list)
 
-class Amount_calculator:
-    def __init__(self):
-        self.
+    def return_menu_amount(self, menu_str):
+        for i in range(len(menu_excel['메뉴명'])):
+            if menu_str == menu_excel['메뉴명'][i]:
+                return self.cart_list.count(menu_str) * menu_excel['가격'][i]
+
+    def return_total_amount(self):
+        total_amount_int = 0
+
+        for i in range(len(set(self.cart_list))):
+            total_amount_int += self.return_menu_amount(list(set(self.cart_list))[i])
+
+        return total_amount_int
+
+    def return_menu_cost(self, menu_str):
+        for i in range(len(menu_excel['메뉴명'])):
+            if menu_str == menu_excel['메뉴명'][i]:
+                return menu_excel['가격'][i]
+
+
+cal = POS_calculator()
+
+
+# create variable in convenience
+menu_names_list = []
+for cell in menu_excel['메뉴명']:
+    menu_names_list.append(cell)
+
+menu_cost_dic = {}
+for i in range(len(menu_excel['메뉴명'])):
+    menu_cost_dic[menu_excel['메뉴명'][i]] = menu_excel['가격'][i]
